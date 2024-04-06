58ca98fcdce8e8a1cb5e4cf998f70dbdc1dc94c6
Michal <mwiselka035@gmail.com>
2024-04-06 19:40:25 +0300

    database connection added

---
+++ project/src/main/java/com/backend/project/domain/Urls.java
@@ -0,0 +1,53 @@
+package com.backend.project.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "urls", indexes = {@Index(name = "domain_index", columnList = "domain")})
+public class Urls {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    private String originalUrl;
+    private String domain;
+    private String shortUrl;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
+}
---
+++ project/src/main/java/com/backend/project/domain/UrlsRepository.java
@@ -0,0 +1,13 @@
+package com.backend.project.domain;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface UrlsRepository {
+
+}
+

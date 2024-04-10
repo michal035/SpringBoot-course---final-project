dfac236cb749de3e8535a1a755ff480eb0fd1e8a
Michal <mwiselka035@gmail.com>
2024-04-10 12:37:07 +0300

    added security configuration

--- project/src/main/java/com/backend/project/ProjectApplication.java
+++ project/src/main/java/com/backend/project/ProjectApplication.java
@@ -1,7 +1,13 @@
 package com.backend.project;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.backend.project.domain.UserRepository;
+import com.backend.project.domain.User;
+
 
 @SpringBootApplication
 public class ProjectApplication {
@@ -10,4 +16,17 @@ public class ProjectApplication {
 		SpringApplication.run(ProjectApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner studentDemo(UserRepository repository) {
+		return (args) -> {
+	
+		// User user1 = new User("user", "$2a$10$eEKLUxUYga1W0dMd1dEZl.HUBtZHOUIEsLhxm9ZHmmndQLRfe1ENS", "USER");
+		// User user2 = new User("admin", "$2a$10$BoHZu7oFYtvW/gxgt410qODS65B7hlU0PAdqCoyS/7PbMuv8/whgi", "ADMIN");
+		// repository.save(user1);
+		// repository.save(user2);
+		
+		
+		};
+	}
 }
--- project/src/main/java/com/backend/project/web/projectController.java
+++ project/src/main/java/com/backend/project/web/projectController.java
@@ -2,13 +2,25 @@ package com.backend.project.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @RestController
 public class projectController {
 
+
+  @GetMapping("/login")
+    public ModelAndView login() {
+        return new ModelAndView("login");
+    }
+
   @GetMapping("/")
   String test() {
+    return "test";
+  }
+
+  @GetMapping("/test")
+  String test2() {
     return "test";
   }
 }
